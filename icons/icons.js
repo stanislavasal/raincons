6ef820f6581650e1b31f2f66aae15da31b462869
Talent <toptalent0411@gmail.com>
2022-10-27 13:19:16 -0400

    gh page folder structure fetch test 4

--- icons/icons.js
+++ icons/icons.js
@@ -44,38 +44,159 @@
   // start polling...
   checkReady(function ($) {
     $(function () {
+      // variables for whole icon category/icons
+      let iconCategories = ['uncategorized']
+      let iconsInCategory = {
+        'uncategorized': []
+      }
+
+      // main fetch function for gh pages
       const fetchSVGIcons = async () => {
-        const repositoryUrl = `https://api.github.com/repos/toptalent0411/raincons/git/trees/main`;
-        const repositoryContent = await fetch(repositoryUrl).then(res => res.json());
-        const iconsFolderObj = repositoryContent.tree.find(node => node.path === 'icons');
+        // find the /icons folder from the main repository
+        const repositoryUrl = `https://api.github.com/repos/toptalent0411/raincons/git/trees/main`
+        const repositoryContent = await fetch(repositoryUrl).then(res => res.json())
+        const iconsFolderObj = repositoryContent.tree.find(node => node.path === 'icons')
+
+        // if /icons folder is found, get all of icon-category/icons
         if (iconsFolderObj) {
           const iconsFolderContent = await fetch(iconsFolderObj.url).then(res => res.json());
-          const iconsFolderNodes = iconsFolderContent.tree.map(node => ({
-            path: node.path,
-            url: node.url
-          }));
-          console.log(iconsFolderNodes)
+          iconsFolderContent.tree.forEach(node => {
+            if (node.path.includes('.')) { // when it's not the icon-category folder
+              // get only valid uncategorized icons
+              if (node.path.search('.svg') != -1) {
+                iconsInCategory['uncategorized'].push(node.path.substring(0, node.path.length - 4))
+              }
+            } else { // this is the case for icon-category folder
+              // save the category
+              iconCategories.push(node.path)
+              iconsInCategory[node.path] = []
+
+              // get the icons inside the category folder
+              const getCategoryIcons = async (category) => {
+                const iconCategoryFolderContent = await fetch(category.url).then(res => res.json())
+                iconCategoryFolderContent.tree.forEach(node => {
+                  // only get the valid svg icons
+                  if (node.path.search('.svg') != -1) {
+                    iconsInCategory[category.path].push(node.path.substring(0, node.path.length - 4))
+                  }
+                })
+              }
+              getCategoryIcons(node)
+            }
+          });
         }
       }
-      const iconCategoryList = fetchSVGIcons('toptalent0411', 'raincons', 'icons')
-      console.log(iconCategoryList)
-      /* async function list_directory(user, repo) {
-        const url = `https://api.github.com/repos/${user}/${repo}/git/trees/main`;
+      fetchSVGIcons()
 
+      console.log(unCategorizedIcons, iconCategories, iconsInCategory)
 
-
-        const dir = await directory.reduce(async (acc, dir) => {
-          const { url } = await acc;
-          const list = await fetch(url).then(res => res.json());
-          return list.tree.find(node => node.path === dir);
-        }, { url });
-        if (dir) {
-          const list = await fetch(dir.url).then(res => res.json());
-          return list.tree.map(node => node.path);
-        }
+      // if it has no uncategorized icon, remove the category from the list 
+      if (iconsInCategory['uncategorized'].length == 0) {
+        iconCategories.shift()
       }
-      list_directory('toptalent0411', 'raincons') */
-      return
+
+      // build the content html for the icon-categories/icons
+      let contentHtml = ``
+      iconCategories.forEach(category => {
+        // build html for the category section
+        contentHtml += `
+          <div class="gap-m direction-row">
+            <!-- Category Label -->
+            <div>
+              <h5 style="text-transform: capitalize;" id="${category}" class="category-label">${category}</h5>
+            </div>
+
+            <!-- SVGIcons -->
+            <div class="gap-l box-l">`
+
+        iconsInCategory[category].forEach(icon => {
+          contentHtml += `
+              <svg-icon class="${category}-icon">${category}/${icon}</svg-icon>
+            `
+        })
+
+        contentHtml += `
+            </div>
+          </div>
+          `
+      })
+
+      // append the built html to the #content
+      $('#content').append(contentHtml)
+
+      // add event handlers after html page is completed
+      // filter when keyboard is released
+      $('#searchInput').on('keyup', function (event) {
+        // display CSS constants
+        const NONE = 'none'
+        const BLOCK = 'block'
+
+        // variables
+        let total_icons = document.getElementsByTagName('svg-icon')
+        let search_content = $(this).val()
+
+        if (search_content === '') {
+          // show all icons
+          for (let i = 0; i < total_icons.length; i++) {
+            $(total_icons[i]).css('display', BLOCK)
+          }
+        } else {
+          // show matched icons except .post-icon
+          for (let i = 0; i < total_icons.length; i++) {
+            if ($(total_icons[i]).hasClass('post-icon')) {
+              continue
+            }
+            let item = total_icons[i].innerHTML
+            if (item.search(search_content) == -1) {
+              $(total_icons[i]).css('display', NONE)
+            } else {
+              $(total_icons[i]).css('display', BLOCK)
+            }
+          }
+        }
+
+        // remove .category-label including no svg-icon
+        $('.category-label').each(function () {
+          let categoryId = $(this)[0].id + '-icon'
+          let flag = false
+          $(document).find(`svg-icon.${categoryId}`).each(function (index, icon) {
+            if ($(icon).css('display') == BLOCK) {
+              flag = true
+            }
+          })
+
+          if (!flag) {
+            $(this).parent().parent().css('display', NONE)
+          } else {
+            $(this).parent().parent().css('display', BLOCK)
+          }
+        })
+      })
+
+      // copy svg-icon iconName to clipboard on clicking 
+      $('svg-icon:not(.post-icon)').click(function () {
+        // get iconName from svg-icon
+        let iconName = $(this)[0].innerHTML
+        let iconHtml = `<svg-icon>${iconName}</svg-icon>`
+
+        // copy iconName to clipboard
+        let dummy = document.createElement("textarea")
+        document.body.appendChild(dummy)
+        dummy.value = iconHtml
+        dummy.select()
+        document.execCommand("copy")
+        document.body.removeChild(dummy)
+
+        // show notification
+        clearTimeout(notificationAppearTimeoutID)
+        $(notification).css('opacity', '1.0')
+        notification.innerHTML = `Copied - &lt;svg-icon&gt;${iconName}&lt;/svg-icon&gt;`
+
+        // hide notification after 3s delay
+        notificationAppearTimeoutID = setTimeout(() => {
+          $(notification).css('opacity', '0.0')
+        }, 3 * 1000)
+      })
 
       /*
       *********************************************** 1. Now JQuery is loaded ********************************************************
